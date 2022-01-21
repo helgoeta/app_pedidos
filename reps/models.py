82218e1b3a574285ac6c427184cae92fe44072af
helen <helen.christine.kds@gmail.com>
2022-01-21 16:44:37 -0300

    Projetando os models empresa, clientes e produtos

--- reps/models.py
+++ reps/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    """Modelo que representa a Empresa"""
+    nome=models.CharField(max_length=200)
+    nome_responsavel=models.CharField(max_length=200)
+    email=models.EmailField
+    data_cricao=models.DateField(auto_now_add=True)
+    ultima_alteracao=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String que representa o Model object"""
+        return self.nome
+
+class Cliente(models.Model):
+    """Modelo que representa o terceiro"""
+    razao_social=models.CharField(max_length=200)
+    data_de_cadastro=models.DateTimeField(auto_now_add=True)
+    ultima_alteracao=models.DateTimeField(auto_now=True)
+    empresa_id=Empresa(id)
+
+    def __str__(self):
+        return self.razao_social
+
+class Produto(models.Model):
+    """Model que representa os produtos"""
+    nome=models.CharField(max_length=200)
+    codigo=models.CharField(max_length=120)
+    tabela_preco=models.DecimalField(max_digits=20, decimal_places=2)
+    multiplo=models.DecimalField(max_digits=7, decimal_places=2)
+    ultima_alteracao=models.DateTimeField(auto_now=True)
+    empresa_id=Empresa(id)
+        
+    def __str__(self):
+        return self.nome
