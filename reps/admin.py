50098f0aae42efe09a971024ea6594e48155a72a
helen <helen.christine.kds@gmail.com>
2022-02-15 12:06:31 -0300

    Registro da classe Admin no models e views no Produto

--- reps/admin.py
+++ reps/admin.py
@@ -1,6 +1,17 @@
+from dataclasses import fields
 from django.contrib import admin
 from reps.models import Empresa, Cliente, Produto
 
-admin.site.register(Empresa)
-admin.site.register(Cliente)
-admin.site.register(Produto)
+# Register the Admin classes for models using the decorator
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nome', 'tabela_preco')
+    fields = ['nome', ('codigo', 'multiplo', 'tabela_preco')]
